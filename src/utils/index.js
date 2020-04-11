9302fe3e354bb8ab16289eb45f12340d6609f5cc
romefar <vlad.romanovr@gmail.com>
2020-04-11 22:32:55 +0300

    added support for date formats

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import trim from './trim'
+import dateFormatter from './dateFormatter'
+import validate from './validate'
+import viewCountFormatter from './viewCountFormatter'
+
+export {
+    trim,
+    dateFormatter,
+    validate,
+    viewCountFormatter
+}
