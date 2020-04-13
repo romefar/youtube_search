bf31b37efb9dbbe6245d9f67ee11335d0ccd5f79
romefar <vlad.romanovr@gmail.com>
2020-04-13 23:48:27 +0300

    updated video list views

--- src/components/video-container/video-container.js
+++ src/components/video-container/video-container.js
@@ -9,22 +9,19 @@ import InfiniteScroll from '../infinite-scroll'
 import LastRowCaption from '../last-row-caption'
 import './video-container.scss'
 import ErrorIndicator from '../error-indicator'
-
+import NoDataIndicator from '../no-data-indicator'
 
 class VideoContainer extends Component { 
 
     loadItems = () => {
         const { query, fetchVideos } = this.props
-        if(query) {
-            fetchVideos(query)
-        }
+        if(query) fetchVideos(query)
     }
 
     render() { 
         const { isLoading, isEnd, hasError, items, query } = this.props
-    
-        if(hasError) return <ErrorIndicator />
         
+        if(hasError) return <ErrorIndicator />
         return query ? 
             <InfiniteScroll 
                 isLoading={isLoading}
@@ -32,13 +29,15 @@ class VideoContainer extends Component {
                 query={query}
                 loader={<Loader />}
                 lastRow={<LastRowCaption />}
+                noData={<NoDataIndicator />}
                 fetch={this.loadItems}
                 items={items}
             /> : null
     }
 }
 
-const mapStateToProps = ({ videoList : { items, isLoading, isEnd, hasError}, search: { query } }) => { 
+const mapStateToProps = ({ videoList : { items, isLoading, isEnd, hasError},
+                           search: { query } }) => { 
     return {
         items,
         isLoading,
--- src/components/video-list/video-list.js
+++ src/components/video-list/video-list.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import VideoListItem from '../video-list-item'
-
+import { v4 as uuidv4 } from 'uuid';
 import './video-list.scss'
 
 const VideoList = (props) => {
     const { items } = props;
     return (
         <div className="video-list">
-            <div className="container">
-                {items.map(item => <VideoListItem key={item.videoData.videoId} item={item} />)}
-            </div>
+            {/* <div className="container"> */}
+                {items.map(item => <VideoListItem key={uuidv4()} item={item} />)}
+            {/* </div> */}
         </div>
     )
 }
