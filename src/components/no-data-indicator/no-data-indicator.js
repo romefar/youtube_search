c950a36696388013be84cf1cd33dd7f7b9e8349e
romefar <vlad.romanovr@gmail.com>
2020-04-13 23:47:00 +0300

    added component for empty result

---
+++ src/components/no-data-indicator/no-data-indicator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '../../icons'
+import './no-data-indicator.scss'
+
+const NoDataIndicator = () => {
+    return (
+        <div className="response-message">
+           <div className="response-message__caption-container"> 
+                <p className="response-message__caption">Sorry we couldn't find any matches for you. </p>
+                <Box className="response-message__logo" />
+           </div>
+        </div>
+    )
+}
+
+export default NoDataIndicator;
