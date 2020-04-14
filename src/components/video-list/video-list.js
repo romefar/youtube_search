fd4546b70eb077e882ed885ec23453d6150cd4a9
romefar <vlad.romanovr@gmail.com>
2020-04-14 18:30:34 +0300

    added adaptive view, bug fixed

--- src/components/video-list/video-list.js
+++ src/components/video-list/video-list.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 import VideoListItem from '../video-list-item'
-import { v4 as uuidv4 } from 'uuid';
+
 import './video-list.scss'
 
 const VideoList = (props) => {
     const { items } = props;
     return (
         <div className="video-list">
-            {/* <div className="container"> */}
-                {items.map(item => <VideoListItem key={uuidv4()} item={item} />)}
-            {/* </div> */}
+            {items.map(item => <VideoListItem key={uuidv4()} item={item} />)}
         </div>
     )
 }
