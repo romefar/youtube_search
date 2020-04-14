72189c013f94b63353f326076267485b7d6a54c2
romefar <vlad.romanovr@gmail.com>
2020-04-14 18:31:23 +0300

    updated utils methods

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,11 +3,13 @@ import dateFormatter from './dateFormatter'
 import validate from './validate'
 import viewCountFormatter from './viewCountFormatter'
 import ResizeController from './resizeController'
+import titleFormatter from './titleFormatter'
 
 export {
     trim,
     dateFormatter,
     validate,
     viewCountFormatter,
-    ResizeController
+    ResizeController,
+    titleFormatter
 }
