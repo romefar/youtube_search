4323e8b7e4580705b32bd36d1550bc13a6e112bb
romefar <vlad.romanovr@gmail.com>
2020-04-16 18:40:31 +0300

    icons/font moved to assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,12 @@
+import { ReactComponent as UpArrow } from './up-arrow.svg'
+import { ReactComponent as YouTubeIcon } from './youtube.svg'
+import { ReactComponent as Finish } from './finish.svg'
+import { ReactComponent as Box } from './box.svg'
+import { ReactComponent as Moon } from './moon.svg'
+export  {
+    UpArrow,
+    YouTubeIcon,
+    Finish,
+    Box,
+    Moon
+}
